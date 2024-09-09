aa85cea52ebe5bc553e9a93ebc88b7a8a68c68ff
陌竹 <mozhu233@outlook.com>
2024-09-08 23:05:58 -0400

    ✨ feat(*): Add Multi Writer

--- vlog.go
+++ vlog.go
@@ -2,6 +2,7 @@ package vlog
 
 import (
 	"fmt"
+	"io"
 	"log/slog"
 	"os"
 )
@@ -10,15 +11,18 @@ var Log = slog.New(NewPrettyHandler(nil))
 
 func G() *slog.Logger { return Log }
 
-func New(filename string) error {
+func New(filename string,ws ...io.Writer) error {
 	f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return err
 	}
+	w := []io.Writer{f}
 	Log = slog.New(NewMultiHandler(
 		NewPrettyHandler(nil),
-		slog.NewJSONHandler(f, &slog.HandlerOptions{Level: slog.LevelDebug, AddSource: true}),
+		slog.NewJSONHandler(
+			io.MultiWriter(append(w,ws...)...),
+			&slog.HandlerOptions{Level: slog.LevelDebug, AddSource: true}),
 	))
 	return nil
 }
